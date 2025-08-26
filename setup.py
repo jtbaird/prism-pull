781672f8d36aef46e6c1122b99cc1a517fb9ae81
John Baird <johnbaird@Johns-MacBook-Air.local>
2025-08-25 20:44:20 -0600

    more CI tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prism-pull",
-    version="0.1.1",
+    version="0.1.2",
     description="Python package to pull PRISM weather data via web automation.",
     author="John Baird",
     packages=find_packages(where="src"),
